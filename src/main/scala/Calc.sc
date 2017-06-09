acf51c4ade596ef764b566de27e03bdc5f62a671
Administrator <Pa$$w0rd>
2017-06-09 09:31:39 +0000

    Blackjack reupload - blackjack - uniquesum - toohot - calc

---
+++ src/main/scala/Calc.sc
@@ -0,0 +1,9 @@
+import scala.collection.mutable.ListBuffer
+
+/**
+  * Created by Administrator on 08/06/2017.
+  */
+object Main {
+
+
+}
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,5 @@
+import scala.collection.mutable.ListBuffer
+
 /**
   * Created by Administrator on 06/06/2017.
   */
@@ -20,6 +22,25 @@ object Main
 
     // TooHot
     println(tooHot(70, false))
+
+    // Calculator
+
+    // Input (3 nums)
+    calc("5, 3, 15")
+    calc("4, 2, 8")
+    calc("6, 2, 12")
+    calc("6, 2, 3")
+    calc("9, 12, 108")
+    calc("5, 3, 15")
+
+    // Input (4 nums)
+    calc("3, 5, 5, 3")
+    calc("2, 4, 6, 3")
+    calc("1, 1, 2, 3")
+    calc("4, 4, 3, 4")
+    calc("8, 4, 3, 6")
+    calc("9, 3, 1, 7")
+    calc("2, 4, 0, 2")
   }
 
   def blackjack (int1: Int, int2: Int): Int =
@@ -71,6 +92,39 @@ object Main
       case(temperature, isSummer) if(temperature >= 60 && temperature <= (if(isSummer) 100 else 90)) => true
       case _ => false
     }
+  }
+
+  def calc(x : String): Unit ={
+    val temp : Array[String] = x.split(", ")
+    var numList : ListBuffer[Int] = ListBuffer()
+
+    for(z <- 0 to temp.size -1){
+      numList+=temp(z).toInt
+
+    }
+
+    for(i<-0 to numList.size -1){
+      var num1 = numList(i%numList.size)
+      var num2 = numList((i+1)%numList.size)
+      var num3 = numList((i+2)%numList.size)
+
+      if(num1 + num2 == num3) {
+        println(num1 + " + " + num2 + " = " + num3)
+      }
+      if(num1 * num2 == num3) {
+        println(num1 + " x " + num2 + " = " + num3)
+      }
+      if(num2!=0)
+        if(num1 / num2 == num3 && num1 % num2 == 0) {
+          println(num1 + " / " + num2 + " = " + num3)
+        }
+      if (num1!= num2)
+      if(num1 - num2 == num3) {
+        println(num1 + " + " + num2 + " = " + num3)
+      }
+
+    }
+    println()
   }
 
 }
